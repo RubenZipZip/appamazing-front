87847fd685a541f9b2ac654e3e3feac260d348de
DESKTOP-UHM3GO0\Usuario\ruben <ruben.cimadevila@gmail.com>
2023-11-08 12:42:24 +0100

    llamadas desde tabla a cada contacto , {{contact.name}} en component.hml

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -16,4 +16,15 @@ export class ContactsService {
     // tipo any para recibir cualquier tipo dato, 
     return this.http.get<any>(url, {headers});
   }
+
+  getContact(c_id: number): Observable<any>{
+    const url = 'http://localhost:30030/contacts/get';
+    const headers =new HttpHeaders().set('Content-Type', 'application/json'); //creo el objeto HttHeaders y le paso parametros
+    // necesitamos desde el header decirle que tipo de dato vamos a recibir Content-type, application/json
+    // pasamos el JSON del body pq es peticion PostMapping en Java que tiene como clave un id
+    // en content-type del Postman nos indica que es un JSON, VALOR PARAMETRO:CLAVE id: c_id
+    const body = JSON.stringify ({id: c_id}); // monta el body tal y como espera recibirlo
+    // es post pq le paso el id del contacto en el body  
+    return this.http.post(url, body,{headers}); //(llama al back no al postman, post pq esta asi en el Controller de java) llama al metodo de back con la url que retorna el body
+  }
 }
